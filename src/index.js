45c236a34d7883d28dbda26c4425c6f98c2df2bc
Rick Clark <rkingsleyclark@gmail.com>
2017-07-23 17:34:41 +0100

    Add electron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Home from './routes/Home';
@@ -12,9 +17,13 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <div>
+        {window.location.pathname.includes('index.html') && <Redirect to="/" />}
         <Route exact path="/" component={Home} />
         <Route exact path="/app" component={App} />
         <Route exact path="/counter" component={Counter} />
+        <Link to="/">Home</Link>
+        <Link to="/app">App</Link>
+        <Link to="/counter">Counter</Link>
       </div>
     </Router>
   </Provider>,
