a33cf50b1b334488c72fdca5f337d12630e30024
Rick Clark <rkingsleyclark@gmail.com>
2017-08-29 13:11:02 +0100

    Remove local storage code to replace w/redux-persist

--- src/routes/Login/actions.js
+++ src/routes/Login/actions.js
@@ -1,10 +1,6 @@
 import { login as types } from '../../actionTypes';
-import localStore from '../../storage/localStore';
 
-export const saveGithubCredentials = credentials => {
-  localStore.insert(credentials);
-  return {
-    type: types.SAVE_GITHUB_CREDENTIALS,
-    credentials,
-  };
-};
+export const saveGithubCredentials = credentials => ({
+  type: types.SAVE_GITHUB_CREDENTIALS,
+  credentials,
+});
--- src/routes/Login/actions.test.js
+++ src/routes/Login/actions.test.js
@@ -1,7 +1,5 @@
 import * as actions from './actions';
 import { login as types } from '../../actionTypes';
-import localStore from '../../storage/localStore';
-import sinon from 'sinon';
 
 describe('Login actions', () => {
   describe('saveGithubCredentials', () => {
@@ -14,12 +12,6 @@ describe('Login actions', () => {
       expect(actions.saveGithubCredentials(credentials)).toEqual(
         expectedAction,
       );
-    });
-    it('saves github credentials to localStorage', () => {
-      const credentials = { id: 'test', secret: 'test' };
-      const storeStub = sinon.stub(localStore, "insert");
-      actions.saveGithubCredentials(credentials);
-      expect(storeStub.calledWith(credentials)).toBe(true);
     });
   });
 });
