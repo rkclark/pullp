700bb9ce72171a7a88c06f07b311d07f306d87a2
Rick Clark <rkingsleyclark@gmail.com>
2017-09-12 13:45:30 +0100

    Add missing reducer tests

--- src/routes/Login/reducer.test.js
+++ src/routes/Login/reducer.test.js
@@ -24,4 +24,35 @@ describe('login reducer', () => {
       expect(newState).toEqual(expectedState);
     });
   });
+  describe('Request Github token failure', () => {
+    it('Saves github failure error', () => {
+      const error = 'error';
+      const expectedState = {
+        ...initialState,
+        loginError: error,
+      };
+
+      const newState = reducer(
+        initialState,
+        actions.requestGithubTokenFailure(error),
+      );
+      expect(newState).toEqual(expectedState);
+    });
+  });
+
+  describe('Request Github token success', () => {
+    it('Saves github token', () => {
+      const token = 'token';
+      const expectedState = {
+        ...initialState,
+        githubToken: token,
+      };
+
+      const newState = reducer(
+        initialState,
+        actions.requestGithubTokenSuccess(token),
+      );
+      expect(newState).toEqual(expectedState);
+    });
+  });
 });
