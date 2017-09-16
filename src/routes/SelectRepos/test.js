267aa5fdff09514dbfcd06c72c8d9eca2002144c
Rick Clark <rkingsleyclark@gmail.com>
2017-09-16 17:16:25 +0100

    In progress: setting up selectRepos connected comp

--- src/routes/SelectRepos/test.js
+++ src/routes/SelectRepos/test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import SelectRepos from './';
+import { shallow, mount } from 'enzyme';
+import { SelectRepos } from './';
 
 describe('SelectRepos', () => {
-  const props = {};
+  const props = {
+    githubError: null,
+    requestWatchedRepos: jest.fn(),
+  };
 
   it('renders successfully', () => {
     const component = shallow(<SelectRepos {...props} />);
     expect(component).toHaveLength(1);
+  });
+
+  it('calls requestWatchedRepos when mounted', () => {
+    const component = mount(<SelectRepos {...props} />);
+    expect(component.requestWatchedRepos).toHaveBeenCalled;
   });
 });
