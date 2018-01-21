466c26b7b9954a052b1148dbc604c134711408b6
Rick Clark <rkingsleyclark@gmail.com>
2018-01-21 12:38:56 +0000

    Style heading loader

--- src/components/CurrentUser/index.js
+++ src/components/CurrentUser/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import defaultTheme from './theme.css';
 
-export default function CurrentUser({ login, avatarUrl, url, theme }) {
+export default function CurrentUser({ avatarUrl, url, theme }) {
   return (
     <div className={theme.currentUser}>
       <a href={url} className={theme.link}>
         <img src={avatarUrl} alt="avatar" className={theme.image} />
       </a>
-      <p className={theme.loggedIn}>{login}</p>
     </div>
   );
 }
 
 CurrentUser.propTypes = {
-  login: PropTypes.string.isRequired,
   avatarUrl: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   theme: PropTypes.shape(),
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,56 +3,24 @@ import PropTypes from 'prop-types';
 
 import defaultTheme from './theme.css';
 
-export default function Loading({ theme }) {
+export default function Loading({ theme, loading }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="30"
-      viewBox="0 0 24 30"
-      className={theme.icon}
-    >
-      <path fill="#333" d="M0 0h4v10H0z">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0"
-          dur="0.6s"
-          repeatCount="indefinite"
-        />
-      </path>
-      <path fill="#333" d="M10 0h4v10h-4z">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0.2s"
-          dur="0.6s"
-          repeatCount="indefinite"
-        />
-      </path>
-      <path fill="#333" d="M20 0h4v10h-4z">
-        <animateTransform
-          attributeType="xml"
-          attributeName="transform"
-          type="translate"
-          values="0 0; 0 20; 0 0"
-          begin="0.4s"
-          dur="0.6s"
-          repeatCount="indefinite"
-        />
-      </path>
-    </svg>
+    <div className={`${theme.loader} ${loading ? theme.loading : null}`}>
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
   );
 }
 
 Loading.propTypes = {
   theme: PropTypes.shape({}),
+  loading: PropTypes.bool,
 };
 
 Loading.defaultProps = {
   theme: defaultTheme,
+  loading: true,
 };
