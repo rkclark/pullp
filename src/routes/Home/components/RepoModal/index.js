e2899c34dfc9737e1d924893ed972a26f24124c5
Rick Clark <rkingsleyclark@gmail.com>
2018-02-04 17:56:49 +0000

    Show message when no open Prs

--- src/routes/Home/components/RepoModal/index.js
+++ src/routes/Home/components/RepoModal/index.js
@@ -43,6 +43,21 @@ export default function RepoModal({ theme, data, toggleOpenRepo }) {
             {data.pullRequests.map(pr => (
               <PullRequest {...pr} key={`${data.id}_${pr.number}`} />
             ))}
+            {data.pullRequests.length === 0 ? (
+              <div className={theme.noPrs}>
+                <svg
+                  className={theme.noPrsIcon}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 31.11 31.11"
+                >
+                  <path
+                    fill="#FFF"
+                    d="M31.1 1.4L29.7 0 15.56 14.14 1.4 0 0 1.4l14.14 14.16L0 29.7l1.4 1.4 14.16-14.13L29.7 31.1l1.4-1.4-14.13-14.14"
+                  />
+                </svg>
+                <span>No open pull requests</span>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
