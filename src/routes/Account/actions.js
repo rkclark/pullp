346b0c4d9535a552a546075bd641f00120a5d46f
Rick Clark <rkingsleyclark@gmail.com>
2018-04-22 15:23:46 +0100

    Update logout action to clear cookies

--- src/routes/Account/actions.js
+++ src/routes/Account/actions.js
@@ -1,9 +1,20 @@
 import { account as types } from '../../actionTypes';
 
+const electron = window.electron;
+
 export const toggleLogoutModal = () => ({
   type: types.TOGGLE_LOGOUT_MODAL,
 });
 
-export const logout = () => ({
+export const clearPersistedLocalStorage = () => ({
   type: types.LOGOUT,
 });
+
+export const logout = () => async dispatch => {
+  // Delete cookies which have been placed there by Github's login page
+  await electron.remote.session.defaultSession.clearStorageData({
+    storages: 'cookies',
+  });
+
+  dispatch(clearPersistedLocalStorage());
+};
