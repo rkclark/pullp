89feffa16172eedb71642a4e0dd3aabfbe554565
Rick Clark <rkingsleyclark@gmail.com>
2018-04-24 16:44:57 +0100

    Remove commented out code in constants.js

--- src/constants.js
+++ src/constants.js
@@ -4,9 +4,4 @@ export const REPO_SUMMARY_MAX_PRS = 3;
 
 export const MAXIMUM_PRS = 50;
 
-// export const SERVER_PORT =
-//   process.env.NODE_ENV === 'production'
-//     ? process.env.REACT_APP_PROD_SERVER_PORT
-//     : process.env.REACT_APP_DEV_SERVER_PORT;
-
 export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
