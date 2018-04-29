cdd268a6c9a6d64625f9385fc99eaad7d5dc4b1e
Rick Clark <rkingsleyclark@gmail.com>
2018-04-29 15:03:23 +0100

    Extract electron menu template to module

---
+++ src/electronHelpers/menuTemplate.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+
+const { app } = electron;
+
+module.exports = [
+  {
+    label: 'Application',
+    submenu: [
+      {
+        label: 'About Application',
+        selector: 'orderFrontStandardAboutPanel:',
+      },
+      { type: 'separator' },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        selector: 'selectAll:',
+      },
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        role: 'zoomin',
+      },
+      {
+        role: 'zoomout',
+      },
+      { role: 'resetzoom' },
+    ],
+  },
+];
