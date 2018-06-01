2da5f0218eb13eb4d27362353b0544ea377b8181
Rick Clark <rkingsleyclark@gmail.com>
2018-06-01 16:45:04 +0100

    Add /authenticate/ route to stub server

--- stubServer/index.js
+++ stubServer/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
-const queryString = require('querystring');
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
@@ -12,34 +10,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/oauth/authorize', async (req, res) => {
-  console.log(req.body);
-
   const htmlPath = path.resolve(__dirname, 'githubLogin.html');
   const content = fs.readFileSync(htmlPath, 'utf8');
-  console.log(content);
+  console.log('Stub server received GET /oauth/authorize');
+  console.log('Stub server returning fake Github sign in page');
   res.send(content);
 });
 
-app.post('/authenticate/', async (req, res) => {
-  console.log(req.body);
-  const oAuthParams = queryString.stringify(req.body);
-  const gitHubResponse = await fetch(
-    `https://github.com/login/oauth/access_token?${oAuthParams}`,
-    {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      } //eslint-disable-line
-  );
-  if (gitHubResponse.ok) {
-    const result = await gitHubResponse.json();
-    res.json(result);
-  } else {
-    console.log(gitHubResponse);
-    res.sendStatus(500);
-  }
+app.get('/authenticate/*', async (req, res) => {
+  console.log('Stub server received GET /authenticate/');
+  console.log('Stub server fake Github token');
+  const response = JSON.stringify({ token: '123456789' });
+  res.send(response);
 });
 
 app.listen(3334, null, () => {
