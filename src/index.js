4ca0ed39920101e617bea792ea1ed7ab5e942075
Rick Clark <rkingsleyclark@gmail.com>
2018-07-20 14:23:17 +0100

    Migrate away from apollo-boost to standard setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default, no-console */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
 import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,7 +17,22 @@ import ScrollToTop from './components/ScrollToTop';
 import './css/index.css';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GITHUB_API_URL,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.REACT_APP_GITHUB_API_URL,
+      credentials: 'same-origin',
+    }),
+  ]),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
