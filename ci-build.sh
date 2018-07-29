b2b704a0221bef6275e7884b241e8e89d5451576
Rick Clark <rkingsleyclark@gmail.com>
2018-07-29 12:11:46 +0100

    Fix ci build config

--- ci-build.sh
+++ ci-build.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -ev
 
-npm install
+npm install --quiet
 
-if [[ "${TRAVIS_BRANCH}" =~ "/^v\d+\.\d+(\.\d+)?(-\S*)?$/" ]]; then
-  if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-      docker run --rm \
+regexp="^v[0-9]+\.[0-9]+\.[0-9]+$"
+
+if [[ "${TRAVIS_BRANCH}" =~ $regexp ]]; then
+    echo "-----> on a version branch"
+    if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+        echo "-----> build for  linux"
+        docker run --rm \
         --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
         -v ${PWD}:/project \
         -v ~/.cache/electron:/root/.cache/electron \
         -v ~/.cache/electron-builder:/root/.cache/electron-builder \
         electronuserland/builder \
-        /bin/bash -c "npm install && npm run dist"
+        /bin/bash -c "npm install && npm run ship"
     else
-      npm run dist
+        echo "-----> build for mac"
+        npm run ship
     fi
 else
-  npm run validate
+    echo "-----> NOT on a version branch"
+    npm run validate
 fi
