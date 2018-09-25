1f19e7af6ac9cb0b50cb25d8eddb51f21f4375f0
Rick Clark <rkingsleyclark@gmail.com>
2018-09-25 16:12:31 +0100

    Add tests for Setup component rendering SignInForm

--- src/routes/SetupNew/index.js
+++ src/routes/SetupNew/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import SignInForm from '../../components/SignInForm';
 
-const GET_GITHUB_TOKEN = gql`
+export const GET_GITHUB_TOKEN_FROM_CACHE = gql`
   {
     githubAuth @client {
       token
@@ -11,31 +13,36 @@ const GET_GITHUB_TOKEN = gql`
   }
 `;
 
-export default function SetupNew() {
+export function SetupNew({ data, client }) {
   return (
-    <Query query={GET_GITHUB_TOKEN}>
-      {({ data, client }) => {
-        console.log('In Query result', data, client);
-        return (
-          <div>
-            {!data.githubAuth.token && (
-              <SignInForm
-                saveGithubToken={token => {
-                  console.log('SAVING TOKEN TO APOLLO', token);
-                  client.writeData({
-                    data: {
-                      githubAuth: {
-                        token,
-                        __typename: 'GithubAuth',
-                      },
-                    },
-                  });
-                }}
-              />
-            )}
-          </div>
-        );
-      }}
+    <div>
+      {!get(data, 'githubAuth.token') && (
+        <SignInForm
+          saveGithubToken={token => {
+            client.writeData({
+              data: {
+                githubAuth: {
+                  token,
+                  __typename: 'GithubAuth',
+                },
+              },
+            });
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+SetupNew.propTypes = {
+  data: PropTypes.shape({}).isRequired,
+  client: PropTypes.shape({}).isRequired,
+};
+
+export default function SetupNewContainer() {
+  return (
+    <Query query={GET_GITHUB_TOKEN_FROM_CACHE} fetchPolicy="cache-only">
+      {({ data, client }) => <SetupNew data={data} client={client} />}
     </Query>
   );
 }
--- src/routes/SetupNew/test.js
+++ src/routes/SetupNew/test.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import ReactRouterEnzymeContext from 'react-router-enzyme-context';
-import { shallow, mount } from 'enzyme';
-import SetupNew from './';
+import { shallow } from 'enzyme';
+import { SetupNew } from './';
 
 import SignInForm from '../../components/SignInForm';
 
 describe('Setup', () => {
+  const defaultProps = {
+    data: {},
+    client: {},
+  };
+
   it('renders successfully', () => {
-    const component = shallow(<SetupNew />);
+    const component = shallow(<SetupNew {...defaultProps} />);
     expect(component.length).toBe(1);
   });
 
-  describe('when path is /setup/sign-in', () => {
-    it('Renders sign in form', () => {
-      const mockRouter = new ReactRouterEnzymeContext({
-        initialEntries: ['/app/setup/sign-in'],
-      });
-      const component = mount(
-        <SetupNew {...mockRouter.props()} />,
-        mockRouter.get(),
-      );
+  describe('when apollo data does not contain github auth token', () => {
+    let component;
+    const clientMock = {
+      writeData: jest.fn(),
+    };
+    const data = {
+      githubAuth: {
+        token: null,
+      },
+    };
+
+    beforeAll(() => {
+      component = shallow(<SetupNew data={data} client={clientMock} />);
+    });
+
+    it('renders a <SignInForm/>', async () => {
       expect(component.find(SignInForm).length).toBe(1);
+    });
+
+    describe('saveGithubToken fn passed to <SignInForm/> as a prop', () => {
+      it('writes github token to apollo client', () => {
+        const token = '1234';
+        const saveGithubToken = component.find(SignInForm).props()
+          .saveGithubToken;
+
+        expect(clientMock.writeData).not.toBeCalled;
+        saveGithubToken(token);
+        expect(clientMock.writeData).toHaveBeenLastCalledWith({
+          data: {
+            githubAuth: {
+              token,
+              __typename: 'GithubAuth',
+            },
+          },
+        });
+      });
     });
   });
 });
