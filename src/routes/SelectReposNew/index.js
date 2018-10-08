709acc6a1e801f8dee983d3182bfcf70b7e7c270
Rick Clark <rkingsleyclark@gmail.com>
2018-10-08 17:28:27 +0100

    Fix SelectReposNew pagination query

--- src/routes/SelectReposNew/index.js
+++ src/routes/SelectReposNew/index.js
@@ -7,72 +7,37 @@ export function SelectReposNew(props) {
   return <div>{JSON.stringify(props)}</div>;
 }
 
-// const testQuery = gql(`query WatchedRepos {
-//   viewer {
-//     watching(first:100, affiliations: [OWNER, COLLABORATOR, ORGANIZATION_MEMBER]) {
-//       edges {
-//         cursor
-//         node {
-//           name
-//           id
-//           url
-//           owner {
-//             login
-//             avatarUrl
-//           }
-//           isFork
-//           createdAt
-//         }
-//       }
-//     }
-//   }
-// }`);
-
-// export default function SelectReposNewContainer() {
-//   return (
-//     <Query query={testQuery} fetchPolicy="network-only">
-//       {({ data, loading }) => {
-//         console.log('data is', data);
-//         console.log('loading is', loading);
-
-//         return <SelectReposNew data={data} loading={loading} />;
-//       }}
-//     </Query>
-//   );
-// }
-
 export default function SelectReposNewContainer() {
   return (
     <Query query={GET_WATCHED_REPOS} fetchPolicy="network-only">
       {({ data, loading, fetchMore }) => {
-        console.log('data is', data);
-        console.log('loading is', loading);
         if (get(data, 'viewer.watching.pageInfo.hasNextPage')) {
           fetchMore({
             variables: {
               cursor: last(data.viewer.watching.edges).cursor,
             },
             updateQuery: (previousResult, { fetchMoreResult }) => {
-              const newEdges = fetchMoreResult.viewer.watching.edges;
-              const pageInfo = fetchMoreResult.viewer.watching.pageInfo;
+              const newEdges = get(fetchMoreResult, 'viewer.watching.edges');
+              const prevEdges = get(previousResult, 'viewer.watching.edges');
+              const pageInfo = get(fetchMoreResult, 'viewer.watching.pageInfo');
 
-              return newEdges.length
+              const mergedResults = newEdges.length
                 ? {
                     // Put the new watched repos at the end of the list and
                     // update `pageInfo` so we have the new hasNextPage value
+                    ...previousResult,
                     viewer: {
+                      ...get(previousResult, 'viewer'),
                       watching: {
-                        // eslint-disable-next-line no-underscore-dangle
-                        __typename: previousResult.viewer.watching.__typename,
-                        edges: [
-                          ...previousResult.viewer.watching.edges,
-                          ...newEdges,
-                        ],
+                        ...get(previousResult, 'viewer.watching'),
+                        edges: [...prevEdges, ...newEdges],
                         pageInfo,
                       },
                     },
                   }
                 : previousResult;
+
+              return mergedResults;
             },
           });
         }
