f80fabe0692fc61e7530745cca406cbe1e553f47
Rick Clark <rkingsleyclark@gmail.com>
2019-01-25 08:36:32 +0000

    Begin testing transformRepos

--- src/routes/HomeNew/transformRepos.js
+++ src/routes/HomeNew/transformRepos.js
@@ -1,16 +1,5 @@
-import { get, orderBy } from 'lodash';
-
-const dateOptions = {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-};
-
-const timeOptions = {
-  hour: 'numeric',
-  minute: 'numeric',
-};
+import { orderBy } from 'lodash';
+import transformPullRequests from './transformPullRequests';
 
 // Maps  Repos
 //   Maps + Normalizes PRs
@@ -28,128 +17,17 @@ const timeOptions = {
 
 // Orders Repos by number of PRs and then name
 
-export default function transform(reposData, userTeamsData) {
+export default function transformRepos(reposData, userTeamsData) {
   let repos = reposData.map(node => {
-    const reformattedPrs = node.pullRequests.edges.map(pr => {
-      const createdAtDate = new Date(pr.node.createdAt);
-      const reviews = pr.node.reviews.edges.map(review => ({
-        ...review.node,
-      }));
-
-      let reviewStatuses = {};
-      if (reviews.length > 0) {
-        reviewStatuses = reviews.reduce((reviewsObj, review) => {
-          if (reviewsObj[review.state]) {
-            return {
-              ...reviewsObj,
-              [review.state]: reviewsObj[review.state] + 1,
-            };
-          }
-          return { ...reviewsObj, [review.state]: 1 };
-        }, {});
-      }
-
-      let reviewsByAuthor = [];
-      if (reviews.length > 0) {
-        reviewsByAuthor = reviews.reduce((arr, review) => {
-          const existingIndex = arr.findIndex(authorReview => {
-            if (authorReview.login === review.author.login) {
-              return true;
-            }
-            return false;
-          });
-
-          if (existingIndex > -1) {
-            arr[existingIndex].states.push(review.state);
-            return [...arr];
-          }
-
-          return [
-            ...arr,
-            {
-              login: review.author.login,
-              avatarUrl: review.author.avatarUrl,
-              states: [review.state],
-            },
-          ];
-        }, []);
-      }
-
-      const reviewRequests = pr.node.reviewRequests.edges.map(
-        reviewRequest => ({
-          ...reviewRequest.node,
-        }),
-      );
-
-      let currentUserReviewRequested = false;
-      let reviewedByCurrentUser = false;
-
-      const userOrganizations = get(
-        userTeamsData,
-        'viewer.organizations.edges',
-      );
-
-      const userTeams = userOrganizations.reduce((teams, { node: org }) => {
-        const additionalTeams = org.teams.edges.map(
-          ({ node: team }) => team.id,
-        );
-
-        return [...teams, ...additionalTeams];
-      }, []);
-
-      if (userTeamsData.viewer.login !== pr.node.author.login) {
-        reviewRequests.some(request => {
-          if (
-            request.requestedReviewer.login &&
-            request.requestedReviewer.login === userTeamsData.viewer.login
-          ) {
-            currentUserReviewRequested = true;
-            return true;
-          }
-          if (
-            request.requestedReviewer.id &&
-            userTeams.some(team => {
-              if (team.id === request.requestedReviewer.id) {
-                return true;
-              }
-              return false;
-            })
-          ) {
-            currentUserReviewRequested = true;
-            return true;
-          }
-          return false;
-        });
-
-        reviews.some(review => {
-          if (review.author.login === userTeamsData.viewer.login) {
-            reviewedByCurrentUser = true;
-            return true;
-          }
-          return false;
-        });
-        if (currentUserReviewRequested && reviewedByCurrentUser) {
-          currentUserReviewRequested = false;
-        }
-      }
-
-      return {
-        ...pr.node,
-        date: createdAtDate.toLocaleDateString('en-GB', dateOptions),
-        time: createdAtDate.toLocaleTimeString('en-US', timeOptions),
-        reviewRequests,
-        reviews,
-        aggregatedReviews: reviewStatuses,
-        currentUserReviewRequested,
-        reviewedByCurrentUser,
-        reviewsByAuthor,
-      };
-    });
+    const transformedPRs = transformPullRequests(
+      node.pullRequests,
+      userTeamsData,
+    );
 
     let currentUserReviewRequests = 0;
     let currentUserReviews = 0;
 
-    reformattedPrs.forEach(pr => {
+    transformedPRs.forEach(pr => {
       if (pr.currentUserReviewRequested) {
         currentUserReviewRequests += 1;
       }
@@ -160,7 +38,7 @@ export default function transform(reposData, userTeamsData) {
 
     return {
       ...node,
-      pullRequests: reformattedPrs,
+      pullRequests: transformedPRs,
       currentUserReviewRequests,
       currentUserReviews,
       totalPullRequests: node.pullRequests.totalCount,
--- src/routes/HomeNew/transformRepos.test.js
+++ src/routes/HomeNew/transformRepos.test.js
@@ -1,7 +1,41 @@
-// import transformRepos from './transformRepos';
+import transformRepos from './transformRepos';
+import transformPullRequests from './transformPullRequests';
+
+const mockPullRequests = [
+  {
+    currentUserReviewRequested: false,
+    reviewedByCurrentUser: true,
+  },
+  {
+    currentUserReviewRequested: true,
+    reviewedByCurrentUser: false,
+  },
+  {
+    currentUserReviewRequested: true,
+    reviewedByCurrentUser: false,
+  },
+  {
+    currentUserReviewRequested: false,
+    reviewedByCurrentUser: false,
+  },
+];
+const mockReposData = [
+  {
+    pullRequests: [],
+  },
+];
+
+jest.mock('./transformPullRequests.js', () => jest.fn());
 
 describe('transformRepos()', () => {
-  it('does stuff', () => {
-    expect(true).toBe(true);
+  let result;
+
+  beforeAll(() => {
+    transformPullRequests.mockImplementation(() => mockPullRequests);
+    result = transformRepos(mockReposData);
+  });
+
+  it("transforms each repo's pull requests data", () => {
+    expect(result[0].pullRequests).toEqual(mockPullRequests);
   });
 });
