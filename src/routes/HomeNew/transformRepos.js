5f796322b7a3ee143960c45b1830edf3f3f1a9b3
Rick Clark <rickclark@bgl-mac-009545.bgl.financial.local>
2019-01-28 08:28:27 +0000

    Unit test transformRepos fn

--- src/routes/HomeNew/transformRepos.js
+++ src/routes/HomeNew/transformRepos.js
@@ -1,22 +1,6 @@
 import { orderBy } from 'lodash';
 import transformPullRequests from './transformPullRequests';
 
-// Maps  Repos
-//   Maps + Normalizes PRs
-//     Makes createdAtDate from createdAt
-//     Sets date and time from createdAtDate
-//     Normalizes reviews array
-//     Creates reviewStatuses object (aggregatedReviews) - NOT USED
-//     Creates reviewsByAuthor array
-//     Normalizes review requests
-//     Determines and sets currentUserReviewRequested
-//     Determines and sets reviewedByCurrentUser
-//   Determines and sets currentUserReviewRequests based on PR content
-//   Determines and sets currentUserReviews based on PR content
-//   Determines and sets totalPullRequests
-
-// Orders Repos by number of PRs and then name
-
 export default function transformRepos(reposData, userTeamsData) {
   let repos = reposData.map(node => {
     const transformedPRs = transformPullRequests(
--- src/routes/HomeNew/transformRepos.test.js
+++ src/routes/HomeNew/transformRepos.test.js
@@ -1,7 +1,7 @@
 import transformRepos from './transformRepos';
 import transformPullRequests from './transformPullRequests';
 
-const mockPullRequests = [
+const testRepoPRs = [
   {
     currentUserReviewRequested: false,
     reviewedByCurrentUser: true,
@@ -19,9 +19,29 @@ const mockPullRequests = [
     reviewedByCurrentUser: false,
   },
 ];
+
+const testRepo2PRs = [{}, {}, {}, {}, {}, {}];
+
+const testRepo3PRs = [{}, {}, {}, {}];
+
+const testRepo4PRs = [{}, {}, {}, {}];
+
 const mockReposData = [
   {
-    pullRequests: [],
+    name: 'testRepo',
+    pullRequests: { totalCount: 4, edges: [{}, {}, {}, {}] },
+  },
+  {
+    name: 'testRepo2',
+    pullRequests: { totalCount: 6, edges: [{}, {}, {}, {}, {}, {}] },
+  },
+  {
+    name: 'testRepo4', // Out of order to show that name sorting works
+    pullRequests: { totalCount: 2, edges: [{}, {}, {}, {}] },
+  },
+  {
+    name: 'testRepo3',
+    pullRequests: { totalCount: 2, edges: [{}, {}, {}, {}] },
   },
 ];
 
@@ -31,11 +51,32 @@ describe('transformRepos()', () => {
   let result;
 
   beforeAll(() => {
-    transformPullRequests.mockImplementation(() => mockPullRequests);
+    transformPullRequests
+      .mockImplementationOnce(() => testRepoPRs)
+      .mockImplementationOnce(() => testRepo2PRs)
+      .mockImplementationOnce(() => testRepo3PRs)
+      .mockImplementationOnce(() => testRepo4PRs);
     result = transformRepos(mockReposData);
   });
 
+  it('sorts repos by number of pull requests and then name', () => {
+    const expectedOrder = ['testRepo2', 'testRepo', 'testRepo3', 'testRepo4'];
+    expect(result.map(repo => repo.name)).toEqual(expectedOrder);
+  });
+
   it("transforms each repo's pull requests data", () => {
-    expect(result[0].pullRequests).toEqual(mockPullRequests);
+    expect(result[1].pullRequests).toEqual(testRepoPRs);
+  });
+
+  it('totals the number of review requests for the current user', () => {
+    expect(result[1].currentUserReviewRequests).toBe(2);
+  });
+
+  it('totals the number of reviews by the current user', () => {
+    expect(result[1].currentUserReviews).toBe(1);
+  });
+
+  it('totals the number of pull requests', () => {
+    expect(result[1].totalPullRequests).toBe(4);
   });
 });
