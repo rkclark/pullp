fbb8bf7d183849bb1633ab3ef24d1559b05410fc
Rick Clark <rkingsleyclark@gmail.com>
2019-02-25 13:00:16 +0000

    Add test for cache cleaner

--- src/apollo/cacheCleaner.test.js
+++ src/apollo/cacheCleaner.test.js
@@ -11,94 +11,231 @@ describe('cleanCacheOnInterval()', () => {
   beforeAll(() => {
     delete window.setInterval;
     window.setInterval = setIntervalMock;
-    cleanCacheOnInterval(client);
-    cleanCacheFn = setIntervalMock.mock.calls[0][0];
+  });
+
+  afterEach(() => {
+    setIntervalMock.mockClear();
   });
 
   it('sets a function to run on an interval', () => {
+    cleanCacheOnInterval(client);
+    cleanCacheFn = setIntervalMock.mock.calls[0][0];
     expect(typeof cleanCacheFn).toBe('function');
   });
 
   describe('interval function', () => {
-    it('deletes all orphaned PullRequest entries', () => {
+    it('deletes all orphaned pull request cache entries', () => {
       const initialCache = {
-        '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
-          totalCount: 4,
-          edges: [
-            {
-              type: 'id',
-              generated: true,
-              id:
-                '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0',
-              typename: 'PullRequestEdge',
+        data: {
+          data: {
+            // Repo 1 - is selected and has one PR
+            'Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==': {
+              name: 'repository-1',
+              isSelected: true,
+              __typename: 'Repository',
+              'pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
+                id:
+                  '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})',
+                typename: 'PullRequestConnection',
+              },
             },
-            {
-              type: 'id',
-              generated: true,
-              id:
-                '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.1',
-              typename: 'PullRequestEdge',
+            // Repo 2 - is NOT selected and has one PR
+            'Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==': {
+              name: 'repository-2',
+              id: 'MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==',
+              url: 'https://github.com/repository-2',
+              isSelected: false,
+              __typename: 'Repository',
             },
-          ],
-          __typename: 'PullRequestConnection',
-        },
-        '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0': {
-          node: {
-            type: 'id',
-            generated: false,
-            id: 'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
-            typename: 'PullRequest',
+            // PR entries for Repo 1
+            '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
+              edges: [
+                {
+                  type: 'id',
+                  generated: true,
+                  id:
+                    '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0',
+                  typename: 'PullRequestEdge',
+                },
+              ],
+              __typename: 'PullRequestConnection',
+            },
+            '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0': {
+              node: {
+                id: 'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+                typename: 'PullRequest',
+              },
+              __typename: 'PullRequestEdge',
+            },
+            'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1': {
+              id: 'MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+              author: {
+                id: '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.author',
+                typename: 'User',
+              },
+              pullpPullRequest: {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.pullpPullRequest',
+                typename: 'PullpPullRequest',
+              },
+              'reviewRequests({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100})',
+                typename: 'ReviewRequestConnection',
+              },
+              'reviews({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviews({"last":100})',
+                typename: 'PullRequestReviewConnection',
+              },
+              __typename: 'PullRequest',
+            },
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.author': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.pullpPullRequest': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100})': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100}).edges.0': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100}).edges.0.node': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviews({"last":100})': {},
+            // PR entries for Repo 2
+            '$Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
+              edges: [
+                {
+                  type: 'id',
+                  generated: true,
+                  id:
+                    '$Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0',
+                  typename: 'PullRequestEdge',
+                },
+              ],
+              __typename: 'PullRequestConnection',
+            },
+            '$Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0': {
+              node: {
+                id: 'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+                typename: 'PullRequest',
+              },
+              __typename: 'PullRequestEdge',
+            },
+            'PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0': {
+              id: 'MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+              author: {
+                id: '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.author',
+                typename: 'User',
+              },
+              pullpPullRequest: {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.pullpPullRequest',
+                typename: 'PullpPullRequest',
+              },
+              'reviewRequests({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviewRequests({"last":100})',
+                typename: 'ReviewRequestConnection',
+              },
+              'reviews({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviews({"last":100})',
+                typename: 'PullRequestReviewConnection',
+              },
+              __typename: 'PullRequest',
+            },
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.author': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.pullpPullRequest': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviewRequests({"last":100})': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviewRequests({"last":100}).edges.0': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviewRequests({"last":100}).edges.0.node': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjQ0ODg1NzM0.reviews({"last":100})': {},
           },
-          __typename: 'PullRequestEdge',
-        },
-        '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.1': {
-          node: {
-            type: 'id',
-            generated: false,
-            id: 'PullRequest:MDExOlB1bGxSZXF1ZXN0MjUyOTkwNTgy',
-            typename: 'PullRequest',
-          },
-          __typename: 'PullRequestEdge',
-        },
-        'Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==': {
-          name: 'rewards.movies-issue-and-redeem',
-          id: 'MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==',
-          url:
-            'https://github.com/ComparetheMarket/rewards.movies-issue-and-redeem',
-          owner: {
-            type: 'id',
-            generated: false,
-            id: 'Organization:MDEyOk9yZ2FuaXphdGlvbjE5NTExMjY=',
-            typename: 'Organization',
-          },
-          isFork: false,
-          createdAt: '2014-10-16T14:57:25Z',
-          isSelected: true,
-          __typename: 'Repository',
-          'pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
-            type: 'id',
-            generated: true,
-            id:
-              '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})',
-            typename: 'PullRequestConnection',
-          },
-        },
-        'Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==': {
-          name: 'rewards.data-bootstrapper',
-          id: 'MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==',
-          url: 'https://github.com/ComparetheMarket/rewards.data-bootstrapper',
-          owner: {
-            type: 'id',
-            generated: false,
-            id: 'Organization:MDEyOk9yZ2FuaXphdGlvbjE5NTExMjY=',
-            typename: 'Organization',
-          },
-          isFork: false,
-          createdAt: '2015-01-10T10:38:06Z',
-          isSelected: false,
-          __typename: 'Repository',
         },
       };
+
+      // Expected cache has all PR content for Repo 2 removed as this repo is not selected
+      const expectedCache = {
+        data: {
+          data: {
+            // Repo 1 - is selected and has one PR
+            'Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==': {
+              name: 'repository-1',
+              isSelected: true,
+              __typename: 'Repository',
+              'pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
+                id:
+                  '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})',
+                typename: 'PullRequestConnection',
+              },
+            },
+            // Repo 2 - is NOT selected and has one PR
+            'Repository:MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==': {
+              name: 'repository-2',
+              id: 'MDEwOlJlcG9zaXRvcnkyOTA1NDE0NA==',
+              url: 'https://github.com/repository-2',
+              isSelected: false,
+              __typename: 'Repository',
+            },
+            // PR entries for Repo 1
+            '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]})': {
+              edges: [
+                {
+                  type: 'id',
+                  generated: true,
+                  id:
+                    '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0',
+                  typename: 'PullRequestEdge',
+                },
+              ],
+              __typename: 'PullRequestConnection',
+            },
+            '$Repository:MDEwOlJlcG9zaXRvcnkyNTMwNzMwMw==.pullRequests({"last":50,"orderBy":{"direction":"DESC","field":"CREATED_AT"},"states":["OPEN"]}).edges.0': {
+              node: {
+                id: 'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+                typename: 'PullRequest',
+              },
+              __typename: 'PullRequestEdge',
+            },
+            'PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1': {
+              id: 'MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1',
+              author: {
+                id: '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.author',
+                typename: 'User',
+              },
+              pullpPullRequest: {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.pullpPullRequest',
+                typename: 'PullpPullRequest',
+              },
+              'reviewRequests({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100})',
+                typename: 'ReviewRequestConnection',
+              },
+              'reviews({"last":100})': {
+                id:
+                  '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviews({"last":100})',
+                typename: 'PullRequestReviewConnection',
+              },
+              __typename: 'PullRequest',
+            },
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.author': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.pullpPullRequest': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100})': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100}).edges.0': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviewRequests({"last":100}).edges.0.node': {},
+            '$PullRequest:MDExOlB1bGxSZXF1ZXN0MjU1MDgwMjU1.reviews({"last":100})': {},
+          },
+        },
+      };
+
+      const cache = { ...initialCache };
+
+      cleanCacheOnInterval({
+        cache,
+      });
+
+      cleanCacheFn = setIntervalMock.mock.calls[0][0];
+
+      cleanCacheFn();
+
+      expect(cache).toEqual(expectedCache);
     });
   });
 });
