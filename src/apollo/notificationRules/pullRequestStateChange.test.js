3084ba3c13e15b31a39630914fa73ca0cfdaa981
Rick Clark <rkingsleyclark@gmail.com>
2019-05-14 16:15:57 +0100

    WIP: PR state notifications

--- src/apollo/notificationRules/pullRequestStateChange.test.js
+++ src/apollo/notificationRules/pullRequestStateChange.test.js
@@ -7,12 +7,16 @@ import {
 
 const { PR_STATE_CHANGE } = notificationTypes;
 
+const { OPENED, RE_OPENED, CLOSED, MERGED } = stateChangeNotificationSubTypes;
+
 const pullRequestId = '123';
 
 const pullRequestTitle = 'A new PR';
 const pullRequestAuthor = 'User';
 
-const pullRequestStateChangeNotification = {
+const currentUser = 'dev';
+
+const pullRequestOpenedNotification = {
   type: PR_STATE_CHANGE,
   subType: stateChangeNotificationSubTypes.OPENED,
   title: 'New Pull Request',
@@ -20,69 +24,34 @@ const pullRequestStateChangeNotification = {
   sourceNodeId: pullRequestId,
 };
 
-const currentUser = 'dev';
+const pullRequestReOpenedNotification = {
+  type: PR_STATE_CHANGE,
+  subType: stateChangeNotificationSubTypes.RE_OPENED,
+  title: 'Pull Request Re-opened',
+  message: `${pullRequestAuthor} re-opened "${pullRequestTitle}"`,
+  sourceNodeId: pullRequestId,
+};
+
+const pullRequestClosedNotification = {
+  type: PR_STATE_CHANGE,
+  subType: stateChangeNotificationSubTypes.CLOSED,
+  title: 'Pull Request Closed',
+  message: `"${pullRequestTitle}"`,
+  sourceNodeId: pullRequestId,
+};
+
+const pullRequestMergedNotification = {
+  type: PR_STATE_CHANGE,
+  subType: stateChangeNotificationSubTypes.MERGED,
+  title: 'Pull Request Merged',
+  message: `"${pullRequestTitle}"`,
+  sourceNodeId: pullRequestId,
+};
 
 describe('pullRequestStateChange', () => {
-  describe(`when there is an existing ${PR_STATE_CHANGE} notification`, () => {
-    it('does not add a new one', () => {
-      const notifications = pullRequestStateChange({
-        currentUser,
-        existingNotifications: [pullRequestStateChangeNotification],
-        pullRequest: {
-          id: pullRequestId,
-          title: pullRequestTitle,
-          state: pullRequestStates.OPEN,
-          author: {
-            login: pullRequestAuthor,
-          },
-        },
-      });
-
-      expect(notifications.length).toBe(0);
-    });
-  });
-
-  describe(`when the PR is not open`, () => {
-    it('does not add a new notification', () => {
-      const notifications = pullRequestStateChange({
-        currentUser,
-        existingNotifications: [],
-        pullRequest: {
-          state: pullRequestStates.CLOSED,
-          id: pullRequestId,
-          title: pullRequestTitle,
-          author: {
-            login: pullRequestAuthor,
-          },
-        },
-      });
-
-      expect(notifications.length).toBe(0);
-    });
-  });
-
-  describe(`when there is no existing ${PR_STATE_CHANGE} notification for the PR`, () => {
-    describe('when the user is the PR author', () => {
-      it('does not add a new notification', () => {
-        const notifications = pullRequestStateChange({
-          currentUser,
-          existingNotifications: [],
-          pullRequest: {
-            id: pullRequestId,
-            state: pullRequestStates.OPEN,
-            title: pullRequestTitle,
-            author: {
-              login: currentUser,
-            },
-          },
-        });
-
-        expect(notifications.length).toBe(0);
-      });
-    });
-
-    describe('when the user is not the PR author', () => {
-      it(`adds a new ${PR_STATE_CHANGE} notification`, () => {
+  describe(`PR ${OPENED} notifications`, () => {
+    describe('when there is no existing state change notification', () => {
+      it(`adds a new ${PR_STATE_CHANGE} notification with subType ${OPENED}`, () => {
         const notifications = pullRequestStateChange({
           currentUser,
           existingNotifications: [],
@@ -96,15 +65,258 @@ describe('pullRequestStateChange', () => {
           },
         });
         expect(notifications.length).toBe(1);
+        const {
+          type,
+          title,
+          message,
+          sourceNodeId,
+          subType,
+        } = notifications[0];
+        expect(type).toBe(pullRequestOpenedNotification.type);
+        expect(title).toBe(pullRequestOpenedNotification.title);
+        expect(message).toBe(pullRequestOpenedNotification.message);
+        expect(subType).toBe(pullRequestOpenedNotification.subType);
+        expect(sourceNodeId).toBe(pullRequestOpenedNotification.sourceNodeId);
+      });
+    });
 
-        const { type, title, message, sourceNodeId } = notifications[0];
+    describe('when there is an existing state change notification', () => {
+      it(`does not add a new ${PR_STATE_CHANGE} notification`, () => {
+        const notifications = pullRequestStateChange({
+          currentUser,
+          existingNotifications: [pullRequestOpenedNotification],
+          pullRequest: {
+            id: pullRequestId,
+            state: pullRequestStates.OPEN,
+            title: pullRequestTitle,
+            author: {
+              login: pullRequestAuthor,
+            },
+          },
+        });
+        expect(notifications.length).toBe(0);
+      });
+    });
+  });
 
-        expect(type).toBe(pullRequestStateChangeNotification.type);
-        expect(title).toBe(pullRequestStateChangeNotification.title);
-        expect(message).toBe(pullRequestStateChangeNotification.message);
-        expect(sourceNodeId).toBe(
-          pullRequestStateChangeNotification.sourceNodeId,
-        );
+  describe(`PR ${CLOSED} notifications`, () => {
+    describe(`when the PR state is ${pullRequestStates.CLOSED}`, () => {
+      describe(`when the most recent state change is not ${CLOSED}`, () => {
+        it(`adds a new ${PR_STATE_CHANGE} notification with subType ${CLOSED}`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestOpenedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.CLOSED,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(1);
+          const {
+            type,
+            title,
+            message,
+            sourceNodeId,
+            subType,
+          } = notifications[0];
+          expect(type).toBe(pullRequestClosedNotification.type);
+          expect(title).toBe(pullRequestClosedNotification.title);
+          expect(message).toBe(pullRequestClosedNotification.message);
+          expect(sourceNodeId).toBe(pullRequestClosedNotification.sourceNodeId);
+          expect(subType).toBe(pullRequestClosedNotification.subType);
+        });
+
+        describe('when the user is the PR author', () => {
+          it('prepends "Your" to the notification title', () => {
+            const notifications = pullRequestStateChange({
+              currentUser,
+              existingNotifications: [pullRequestOpenedNotification],
+              pullRequest: {
+                id: pullRequestId,
+                state: pullRequestStates.CLOSED,
+                title: pullRequestTitle,
+                author: {
+                  login: currentUser,
+                },
+              },
+            });
+            expect(notifications.length).toBe(1);
+            const {
+              type,
+              title,
+              message,
+              sourceNodeId,
+              subType,
+            } = notifications[0];
+            expect(type).toBe(pullRequestClosedNotification.type);
+            expect(title).toBe(`Your ${pullRequestClosedNotification.title}`);
+            expect(message).toBe(pullRequestClosedNotification.message);
+            expect(sourceNodeId).toBe(
+              pullRequestClosedNotification.sourceNodeId,
+            );
+            expect(subType).toBe(pullRequestClosedNotification.subType);
+          });
+        });
+      });
+
+      describe(`when the most recent state change is ${CLOSED}`, () => {
+        it(`does not add a new ${PR_STATE_CHANGE} notification`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestClosedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.CLOSED,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(0);
+        });
+      });
+    });
+  });
+
+  describe(`PR ${MERGED} notifications`, () => {
+    describe(`when the PR state is ${pullRequestStates.MERGED}`, () => {
+      describe(`when the most recent state change is not ${MERGED}`, () => {
+        it(`adds a new ${PR_STATE_CHANGE} notification with subType ${MERGED}`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestOpenedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.MERGED,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(1);
+          const {
+            type,
+            title,
+            message,
+            sourceNodeId,
+            subType,
+          } = notifications[0];
+          expect(type).toBe(pullRequestMergedNotification.type);
+          expect(title).toBe(pullRequestMergedNotification.title);
+          expect(message).toBe(pullRequestMergedNotification.message);
+          expect(sourceNodeId).toBe(pullRequestMergedNotification.sourceNodeId);
+          expect(subType).toBe(pullRequestMergedNotification.subType);
+        });
+
+        describe('when the user is the PR author', () => {
+          it('prepends "Your" to the notification title', () => {
+            const notifications = pullRequestStateChange({
+              currentUser,
+              existingNotifications: [pullRequestOpenedNotification],
+              pullRequest: {
+                id: pullRequestId,
+                state: pullRequestStates.MERGED,
+                title: pullRequestTitle,
+                author: {
+                  login: currentUser,
+                },
+              },
+            });
+            expect(notifications.length).toBe(1);
+            const {
+              type,
+              title,
+              message,
+              sourceNodeId,
+              subType,
+            } = notifications[0];
+            expect(type).toBe(pullRequestMergedNotification.type);
+            expect(title).toBe(`Your ${pullRequestMergedNotification.title}`);
+            expect(message).toBe(pullRequestMergedNotification.message);
+            expect(sourceNodeId).toBe(
+              pullRequestMergedNotification.sourceNodeId,
+            );
+            expect(subType).toBe(pullRequestMergedNotification.subType);
+          });
+        });
+      });
+
+      describe(`when the most recent state change is ${MERGED}`, () => {
+        it(`does not add a new ${PR_STATE_CHANGE} notification`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestMergedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.MERGED,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(0);
+        });
+      });
+    });
+  });
+
+  describe(`PR ${RE_OPENED} notifications`, () => {
+    describe(`when the PR state is ${pullRequestStates.OPEN}`, () => {
+      describe(`when the most recent state change is not ${OPENED}`, () => {
+        it(`adds a new ${PR_STATE_CHANGE} notification with subType ${RE_OPENED}`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestClosedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.OPEN,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(1);
+          const {
+            type,
+            title,
+            message,
+            sourceNodeId,
+            subType,
+          } = notifications[0];
+          expect(type).toBe(pullRequestReOpenedNotification.type);
+          expect(title).toBe(pullRequestReOpenedNotification.title);
+          expect(message).toBe(pullRequestReOpenedNotification.message);
+          expect(sourceNodeId).toBe(
+            pullRequestReOpenedNotification.sourceNodeId,
+          );
+          expect(subType).toBe(pullRequestReOpenedNotification.subType);
+        });
+      });
+
+      describe(`when the most recent state change is ${RE_OPENED}`, () => {
+        it(`does not add a new ${PR_STATE_CHANGE} notification`, () => {
+          const notifications = pullRequestStateChange({
+            currentUser,
+            existingNotifications: [pullRequestReOpenedNotification],
+            pullRequest: {
+              id: pullRequestId,
+              state: pullRequestStates.OPEN,
+              title: pullRequestTitle,
+              author: {
+                login: pullRequestAuthor,
+              },
+            },
+          });
+          expect(notifications.length).toBe(1);
+        });
       });
     });
   });
