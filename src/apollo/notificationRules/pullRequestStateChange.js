5d37923da8ad93f907071eae14c68fd91c965fe1
Rick Clark <rkingsleyclark@gmail.com>
2019-05-14 16:15:57 +0100

    WIP: PR state notifications

--- src/apollo/notificationRules/pullRequestStateChange.js
+++ src/apollo/notificationRules/pullRequestStateChange.js
@@ -6,36 +6,87 @@ import {
 } from '../../constants';
 
 export default ({ existingNotifications, pullRequest, currentUser }) => {
-  const newNotifications = [];
-
   const prAuthor = get(pullRequest, 'author.login');
+  const isUserPRAuthor = currentUser === prAuthor;
 
-  if (
-    prAuthor === currentUser ||
-    pullRequest.state !== pullRequestStates.OPEN
-  ) {
-    return newNotifications;
-  }
+  const mostRecentState = existingNotifications.reduce(
+    (state, notification) => {
+      if (notification.type === notificationTypes.PR_STATE_CHANGE) {
+        return notification.subType;
+      }
 
-  const hasPROpeningAlreadyBeenNotified = () =>
-    existingNotifications.find(
-      notification => notification.type === notificationTypes.PR_STATE_CHANGE,
-    );
+      return state;
+    },
+    null,
+  );
 
-  if (!hasPROpeningAlreadyBeenNotified()) {
-    const message = `${get(pullRequest, 'author.login')} opened "${
-      pullRequest.title
-    }"`;
+  if (!mostRecentState) {
+    const message = `${prAuthor} opened "${pullRequest.title}"`;
     const title = 'New Pull Request';
 
-    newNotifications.push({
-      type: notificationTypes.PR_STATE_CHANGE,
-      subType: stateChangeNotificationSubTypes.OPENED,
-      title,
-      message,
-      sourceNodeId: pullRequest.id,
-    });
+    return [
+      {
+        type: notificationTypes.PR_STATE_CHANGE,
+        subType: stateChangeNotificationSubTypes.OPENED,
+        title,
+        message,
+        sourceNodeId: pullRequest.id,
+      },
+    ];
   }
 
-  return newNotifications;
+  const pullRequestState = pullRequest.state;
+
+  if (pullRequestState === pullRequestStates.CLOSED) {
+    if (mostRecentState !== stateChangeNotificationSubTypes.CLOSED) {
+      const message = `"${pullRequest.title}"`;
+      const title = `${isUserPRAuthor ? 'Your ' : ''}Pull Request Closed`;
+
+      return [
+        {
+          type: notificationTypes.PR_STATE_CHANGE,
+          subType: stateChangeNotificationSubTypes.CLOSED,
+          title,
+          message,
+          sourceNodeId: pullRequest.id,
+        },
+      ];
+    }
+  }
+
+  if (pullRequestState === pullRequestStates.MERGED) {
+    if (mostRecentState !== stateChangeNotificationSubTypes.MERGED) {
+      const message = `"${pullRequest.title}"`;
+      const title = `${isUserPRAuthor ? 'Your ' : ''}Pull Request Merged`;
+
+      return [
+        {
+          type: notificationTypes.PR_STATE_CHANGE,
+          subType: stateChangeNotificationSubTypes.MERGED,
+          title,
+          message,
+          sourceNodeId: pullRequest.id,
+        },
+      ];
+    }
+  }
+
+  if (pullRequestState === pullRequestStates.OPEN) {
+    if (mostRecentState !== stateChangeNotificationSubTypes.OPENED) {
+      const message = `${prAuthor} re-opened "${pullRequest.title}"`;
+      const title = 'Pull Request Re-opened';
+
+      return [
+        {
+          type: notificationTypes.PR_STATE_CHANGE,
+          subType: stateChangeNotificationSubTypes.RE_OPENED,
+          title,
+          message,
+          sourceNodeId: pullRequest.id,
+        },
+      ];
+    }
+  }
+
+  return [];
 };
