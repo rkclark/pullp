aa98d295652cffe5d1c982ef537d55e5a0ca140d
Rick Clark <rkingsleyclark@gmail.com>
2019-05-19 15:27:54 +0100

    Update tick and cross icons

--- src/components/TickIcon/index.js
+++ src/components/TickIcon/index.js
@@ -4,16 +4,11 @@ import PropTypes from 'prop-types';
 export default function TickIcon({ theme }) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 406.83 406.83"
-      width="512"
-      height="512"
       className={theme.svg}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 612 612"
     >
-      <path
-        fill="#0f0d1d"
-        d="M385.62 62.5l-239.4 239.4-125-125L0 198.1l146.22 146.23L406.84 83.72z"
-      />
+      <path d="M595.6 81.55a56.03 56.03 0 0 0-79.25 0L183.03 414.87 94.4 338.74a55.3 55.3 0 0 0-78.2 78.2l132.4 113.74a55.3 55.3 0 0 0 78.21 0 55.88 55.88 0 0 0 5.72-6.99c.32-.3.67-.5.99-.8l362.08-362.1a56.04 56.04 0 0 0 0-79.24z" />
     </svg>
   );
 }
