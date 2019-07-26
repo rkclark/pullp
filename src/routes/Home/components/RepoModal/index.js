134670fd9396b5115f945fb1e56081e1ef2172c4
Rick Clark <rkingsleyclark@gmail.com>
2019-07-26 09:12:30 +0100

    Style no PR messages

--- src/routes/Home/components/RepoModal/index.js
+++ src/routes/Home/components/RepoModal/index.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import defaultTheme from './theme.css';
 import PullRequest from '../PullRequest';
+import CrossIcon from '../../../../components/CrossIcon';
 
 export default class RepoModal extends Component {
   componentDidMount() {
@@ -52,25 +53,16 @@ export default class RepoModal extends Component {
             </a>
           </div>
           <div className={theme.mainContainer}>
+            {data.pullRequests.length === 0 && (
+              <div className={theme.noPRs}>
+                <CrossIcon theme={{ svg: theme.crossIcon }} />
+                <p>No open pull requests</p>
+              </div>
+            )}
             <div className={theme.pullRequestsContainer}>
               {data.pullRequests.map(pr => (
                 <PullRequest {...pr} key={`${data.id}_${pr.number}`} />
               ))}
-              {data.pullRequests.length === 0 && (
-                <div className={theme.noPrs}>
-                  <svg
-                    className={theme.noPrsIcon}
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 31.11 31.11"
-                  >
-                    <path
-                      fill="#FFF"
-                      d="M31.1 1.4L29.7 0 15.56 14.14 1.4 0 0 1.4l14.14 14.16L0 29.7l1.4 1.4 14.16-14.13L29.7 31.1l1.4-1.4-14.13-14.14"
-                    />
-                  </svg>
-                  <span>No open pull requests</span>
-                </div>
-              )}
             </div>
           </div>
         </div>
